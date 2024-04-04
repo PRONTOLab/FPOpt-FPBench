c99d13d07751873395ff0a6688266e5492d7ed8b
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-04-04 23:16:45 +0900

    Different args for each position

--- driver.cpp
+++ driver.cpp
@@ -6,51 +6,62 @@
 #include <vector>
 
 constexpr unsigned INPUT_NUM = 1;
+constexpr unsigned MAX_ARG_NUM = 50;
 
 template <typename T,
           std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
-__attribute__((noinline)) std::vector<T> inputs() {
+std::vector<std::vector<T>> &inputs() {
   static auto v = []() {
-    std::vector<T> a;
-    for (unsigned i = 0; i < INPUT_NUM; i++) {
-      a.push_back(((T)rand()) / 1000);
+    std::vector<std::vector<T>> v;
+    for (unsigned i = 0; i < MAX_ARG_NUM; i++) {
+        std::vector<T> a;
+        for (unsigned i = 0; i < INPUT_NUM; i++) {
+            a.push_back(((T)(rand())) / 1000);
+        }
+        v.push_back(a);
     }
-    return a;
+    return v;
   }();
   return v;
 }
 
 template <typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
-__attribute__((noinline)) std::vector<T> inputs() {
+std::vector<std::vector<T>> &inputs() {
   static auto v = []() {
-    std::vector<T> a;
-    for (unsigned i = 0; i < INPUT_NUM; i++) {
-      a.push_back(rand() % 100);
+    std::vector<std::vector<T>> v;
+    for (unsigned i = 0; i < MAX_ARG_NUM; i++) {
+        std::vector<T> a;
+        for (unsigned i = 0; i < INPUT_NUM; i++) {
+            a.push_back(((T)rand()) % 100 + 1);
+        }
+        v.push_back(a);
     }
-    return a;
+    return v;
   }();
   return v;
 }
 
 template <typename t>
-void run(std::function<t()> f, const char *name, std::string argstr) {
-  std::cout << name << "(" << argstr << ") = " << std::setprecision(15) << f()
+void run(std::function<t()> f, const char *name, std::string argstr, unsigned i) {
+  std::cout << name << "(" << argstr.c_str() + 2 << ") = " << std::setprecision(15) << f()
             << std::endl;
 }
 
 template <typename t, typename arg, typename... args>
 void run(std::function<t(arg, args...)> f, const char *name,
-         std::string argstr) {
-  for (arg i : inputs<arg>()) {
+         std::string argstr, unsigned argno) {
+  for (arg i : inputs<arg>()[argno]) {
     run(std::function<t(args...)>([=](args... a) { return f(i, a...); }), name,
-        argstr + ", " + std::to_string(i));
+        argstr + ", " + std::to_string(i), argno + 1);
   }
 }
 
 template <typename t, typename... args>
-void run2(t (*f)(args...), const char *name) {
-  run(std::function<t(args...)>(f), name, "");
+__attribute__((noinline)) void run2(t (*f)(args...), const char *name) {
+  run(std::function<t(args...)>(f), name, "", 0);
   printf("\n");
 }
+
+void init() { srand(1); }
 
 #define RUN(arg) run2(arg, #arg);
--- run-all.sh
+++ run-all.sh
@@ -17,16 +17,16 @@ function handle() {
     NUM="$(grep -e ' ex[0-9]*(' -- $SRC | wc -l)"
 
     echo "int main() {" >> "$NEWDRIVER"
-    echo "    srand(1);" >> "$NEWDRIVER"
+    echo "    init();" >> "$NEWDRIVER"
     for i in $(seq 0 $(( $NUM - 1 ))); do
         echo "    RUN(ex$i)" >> "$NEWDRIVER"
     done
     echo "    return 0;" >> "$NEWDRIVER"
     echo "}" >> "$NEWDRIVER"
 
-    clang++ "$NEWDRIVER" -include "$SRC" -o "$EXE" $CXXFLAGS
-    "$EXE"
+    clang++ -Wall "$NEWDRIVER" -g -include "$SRC" -o "$EXE" $CXXFLAGS 1>2
 
+    "$EXE"
 }
 
 for f in $(find . -name '*.fpcore.c'); do
