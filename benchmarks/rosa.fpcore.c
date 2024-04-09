7c8fa48e88307c7d1692a6e31d7e76718dab926f
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2024-04-09 07:33:16 +0200

    Disable some benchmarks which fail on mpfr

--- benchmarks/rosa.fpcore.c
+++ benchmarks/rosa.fpcore.c
@@ -259,27 +259,29 @@ double ex34(double x0, double y0, double z0, double vx0, double vy0, double vz0)
 }
 
 double ex35(double t0, double w0, double N) {
-	double h = 0.01;
-	double L = 2.0;
-	double m = 1.5;
-	double g = 9.80665;
-	double t = t0;
-	double w = w0;
-	double n = 0.0;
-	int tmp = n < N;
-	while (tmp) {
-		double k1w = (-g / L) * sin(t);
-		double k2t = w + ((h / 2.0) * k1w);
-		double t_1 = t + (h * k2t);
-		double k2w = (-g / L) * sin((t + ((h / 2.0) * w)));
-		double w_2 = w + (h * k2w);
-		double n_3 = n + 1.0;
-		t = t_1;
-		w = w_2;
-		n = n_3;
-		tmp = n < N;
-	}
-	return t;
+	// TODO hangs with mpfr
+	// double h = 0.01;
+	// double L = 2.0;
+	// double m = 1.5;
+	// double g = 9.80665;
+	// double t = t0;
+	// double w = w0;
+	// double n = 0.0;
+	// int tmp = n < N;
+	// while (tmp) {
+	// 	double k1w = (-g / L) * sin(t);
+	// 	double k2t = w + ((h / 2.0) * k1w);
+	// 	double t_1 = t + (h * k2t);
+	// 	double k2w = (-g / L) * sin((t + ((h / 2.0) * w)));
+	// 	double w_2 = w + (h * k2w);
+	// 	double n_3 = n + 1.0;
+	// 	t = t_1;
+	// 	w = w_2;
+	// 	n = n_3;
+	// 	tmp = n < N;
+	// }
+	// return t;
+	return 0;
 }
 
 double ex36(double x0) {
