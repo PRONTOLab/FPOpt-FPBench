67092e2ab381b9ca40985cb6cdc948778f3273c1
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2024-04-14 04:03:51 +0900

    Add timeout

--- run-one.sh
+++ run-one.sh
@@ -7,20 +7,18 @@ SRC="$1"
 echo Handling "$SRC"
 
 NEWDRIVER="$SRC.driver.cpp"
-cp driver.cpp "$NEWDRIVER"
 
 AOUT="$SRC.a.out"
 
 NUM="$(grep -e ' ex[0-9]*(' -- $SRC | wc -l)"
 
-echo "int main() {" >> "$NEWDRIVER"
-echo "    init();" >> "$NEWDRIVER"
 for i in $(seq 0 $(( $NUM - 1 ))); do
+    cp driver.cpp "$NEWDRIVER"
+    echo "int main() {" >> "$NEWDRIVER"
+    echo "    init();" >> "$NEWDRIVER"
     echo "    RUN(ex$i)" >> "$NEWDRIVER"
+    echo "    return 0;" >> "$NEWDRIVER"
+    echo "}" >> "$NEWDRIVER"
+    clang++ -Wall "$NEWDRIVER" -g -include "$SRC" -o "$AOUT" $CXXFLAGS 1>2
+    timeout -k 1s 5s "$AOUT" || echo TIMEOUT OR FAIL
 done
-echo "    return 0;" >> "$NEWDRIVER"
-echo "}" >> "$NEWDRIVER"
-
-clang++ -Wall "$NEWDRIVER" -g -include "$SRC" -o "$AOUT" $CXXFLAGS 1>2
-
-"$AOUT"
