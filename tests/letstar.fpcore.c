436ac9b9e91890d18e3387894c9ad8deefd58425
Ivan Radanov Ivanov <ivanradanov@yahoo.co.uk>
2024-04-14 03:59:03 +0900

    fix

--- tests/letstar.fpcore.c
+++ tests/letstar.fpcore.c
@@ -1,6 +1,7 @@
 #include <fenv.h>
 #include <math.h>
 #include <stdint.h>
+#include <stdio.h>
 #define TRUE 1
 #define FALSE 0
 
@@ -23,32 +24,28 @@ double ex2(double a, double b) {
 }
 
 double ex3(double a) {
-	// TODO hangs on mpfr
-	// double c = 0.0;
-	// double d = 0.0;
-	// int tmp = c < a;
-	// while (tmp) {
-	// 	double c_1 = 1.0 + c;
-	// 	double d_2 = d + c;
-	// 	c = c_1;
-	// 	d = d_2;
-	// 	tmp = c < a;
-	// }
-	// return d;
-	return 0;
+	double c = 0.0;
+	double d = 0.0;
+	int tmp = c < a;
+	while (tmp) {
+		double c_1 = 1.0 + c;
+		double d_2 = d + c;
+		c = c_1;
+		d = d_2;
+		tmp = c < a;
+	}
+	return d;
 }
 
 double ex4(double a) {
-	// TODO hangs on mpfr
-	// double c = 0.0;
-	// double d = 0.0;
-	// int tmp = c < a;
-	// while (tmp) {
-	// 	c = 1.0 + c;
-	// 	d = d + c;
-	// 	tmp = c < a;
-	// }
-	// return d;
-	return 0;
+	double c = 0.0;
+	double d = 0.0;
+	int tmp = c < a;
+	while (tmp) {
+		c = 1.0 + c;
+		d = d + c;
+		tmp = c < a;
+	}
+	return d;
 }
 
