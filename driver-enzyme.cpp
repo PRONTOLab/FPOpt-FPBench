651b954a724e1755b568ad3bbbff12be8a41e368
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-04-25 15:28:41 +0900

    driver-enzyme

---
+++ driver-enzyme.cpp
@@ -0,0 +1,79 @@
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include <enzyme/fprt/fprt.h>
+
+#define FROM 64
+#define TO 32
+
+constexpr unsigned INPUT_NUM = 1;
+constexpr unsigned MAX_ARG_NUM = 50;
+
+template <typename T,
+          std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
+std::vector<std::vector<T>> &inputs() {
+  static auto v = []() {
+    std::vector<std::vector<T>> v;
+    for (unsigned i = 0; i < MAX_ARG_NUM; i++) {
+      std::vector<T> a;
+      for (unsigned i = 0; i < INPUT_NUM; i++) {
+        a.push_back(((T)(rand())) / 1000);
+      }
+      v.push_back(a);
+    }
+    return v;
+  }();
+  return v;
+}
+
+template <typename T, std::enable_if_t<std::is_integral<T>::value, bool> = true>
+std::vector<std::vector<T>> &inputs() {
+  static auto v = []() {
+    std::vector<std::vector<T>> v;
+    for (unsigned i = 0; i < MAX_ARG_NUM; i++) {
+      std::vector<T> a;
+      for (unsigned i = 0; i < INPUT_NUM; i++) {
+        a.push_back(((T)rand()) % 100 + 1);
+      }
+      v.push_back(a);
+    }
+    return v;
+  }();
+  return v;
+}
+
+template <typename t>
+void run(std::function<t()> f, const char *name, std::string argstr,
+         unsigned i) {
+  auto res = __enzyme_expand_mem_value_d(f(), FROM, TO);
+  std::cout << name << "(" << (i == 0 ? "" : argstr.c_str() + 2)
+            << ") = " << std::setprecision(15) << res << std::endl;
+}
+
+template <typename t, typename arg, typename... args>
+void run(std::function<t(arg, args...)> f, const char *name, std::string argstr,
+         unsigned argno) {
+  for (arg i : inputs<arg>()[argno]) {
+    run(std::function<t(args...)>([=](args... a) {
+          auto ti = __enzyme_truncate_mem_value_d(i, FROM, TO);
+          return f(ti, a...);
+        }),
+        name, argstr + ", " + std::to_string(i), argno + 1);
+  }
+}
+
+template <typename fty> fty *__enzyme_truncate_mem_func(fty *, int, int);
+
+template <typename t, typename... args>
+__attribute__((noinline)) void run2(t (*f)(args...), const char *name) {
+  run(std::function<t(args...)>(f), name, "", 0);
+  printf("\n");
+}
+
+void init() { srand(1); }
+
+#define RUN(arg) run2(__enzyme_truncate_mem_func(arg, FROM, TO), #arg);
