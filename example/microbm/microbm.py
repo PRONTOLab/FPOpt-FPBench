0df3b8cf96150aa094925247635d1a2a9c9daa03
Brant-Skywalker <scharfrichterq@gmail.com>
2024-09-28 17:29:21 -0500

    fix up

--- example/microbm/microbm.py
+++ example/microbm/microbm.py
@@ -6,11 +6,14 @@ import struct
 import numpy as np
 import random
 
+random.seed(42)
+
 instructions = ["fneg", "fadd", "fsub", "fmul", "fdiv", "fcmp", "fptrunc", "fpext"]
 functions = ["sin", "cos", "tan", "exp", "log", "sqrt", "expm1", "log1p", "cbrt", "pow", "fma", "fabs", "hypot"]
 
 precisions = ["double", "float", "half"]
-iterations = 100000000
+iterations = 1000000000
+unrolled = 100
 
 precision_to_llvm_type = {
     "double": "double",
@@ -69,6 +72,8 @@ def generate_random_fp(precision):
 
 
 def generate_baseline_code(iterations):
+    random_fps = [generate_random_fp("double") for _ in range(unrolled)]
+    hex_fps = [float_to_llvm_hex(f, "double") for f in random_fps]
     code = f"""
 define i32 @main() {{
 entry:
@@ -100,10 +105,8 @@ def generate_llvm_code(instruction, precision, iterations):
     if instruction in ["fadd", "fsub", "fmul", "fdiv"]:
         op_map = {"fadd": "fadd", "fsub": "fsub", "fmul": "fmul", "fdiv": "fdiv"}
         op = op_map[instruction]
-        a = generate_random_fp(precision)
-        b = generate_random_fp(precision)
-        hex_a = float_to_llvm_hex(a, precision)
-        hex_b = float_to_llvm_hex(b, precision)
+        random_pairs = [(generate_random_fp(precision), generate_random_fp(precision)) for _ in range(unrolled)]
+        hex_pairs = [(float_to_llvm_hex(a, precision), float_to_llvm_hex(b, precision)) for a, b in random_pairs]
         code = f"""
 define i32 @main() {{
 entry:
@@ -119,10 +122,15 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %result = {op} fast {llvm_type} {hex_a}, {hex_b}
-  %acc_val = load {llvm_type}, {llvm_type}* %acc
-  %new_acc = fadd fast {llvm_type} %acc_val, %result
-  store {llvm_type} %new_acc, {llvm_type}* %acc
+  %acc_val0 = load {llvm_type}, {llvm_type}* %acc
+"""
+
+        for idx, (hex_a, hex_b) in enumerate(hex_pairs):
+            code += f"  %result{idx} = {op} fast {llvm_type} {hex_a}, {hex_b}\n"
+            code += f"  %acc_val{idx+1} = fadd fast {llvm_type} %acc_val{idx}, %result{idx}\n"
+
+        code += f"""
+  store {llvm_type} %acc_val{len(hex_pairs)}, {llvm_type}* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
@@ -138,8 +146,8 @@ define void @use({llvm_type} %val) {{
 }}
 """
     elif instruction == "fneg":
-        a = generate_random_fp(precision)
-        hex_a = float_to_llvm_hex(a, precision)
+        random_fps = [generate_random_fp(precision) for _ in range(unrolled)]
+        hex_fps = [float_to_llvm_hex(f, precision) for f in random_fps]
         code = f"""
 define i32 @main() {{
 entry:
@@ -155,10 +163,15 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %result = fneg fast {llvm_type} {hex_a}
-  %acc_val = load {llvm_type}, {llvm_type}* %acc
-  %new_acc = fadd fast {llvm_type} %acc_val, %result
-  store {llvm_type} %new_acc, {llvm_type}* %acc
+  %acc_val0 = load {llvm_type}, {llvm_type}* %acc
+"""
+
+        for idx, hex_a in enumerate(hex_fps):
+            code += f"  %result{idx} = fneg fast {llvm_type} {hex_a}\n"
+            code += f"  %acc_val{idx+1} = fadd fast {llvm_type} %acc_val{idx}, %result{idx}\n"
+
+        code += f"""
+  store {llvm_type} %acc_val{len(hex_fps)}, {llvm_type}* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
@@ -174,17 +187,13 @@ define void @use({llvm_type} %val) {{
 }}
 """
     elif instruction == "fcmp":
-        a = generate_random_fp(precision)
-        b = generate_random_fp(precision)
-        hex_a = float_to_llvm_hex(a, precision)
-        hex_b = float_to_llvm_hex(b, precision)
         code = f"""
 define i32 @main() {{
 entry:
   %i = alloca i32
-  %acc = alloca i1
+  %acc = alloca i32
   store i32 0, i32* %i
-  store i1 false, i1* %acc
+  store i32 0, i32* %acc
   br label %loop
 
 loop:
@@ -193,19 +202,34 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %cmp = fcmp fast olt {llvm_type} {hex_a}, {hex_b}
-  store i1 %cmp, i1* %acc
+"""
+
+        for idx in range(unrolled):
+            a = generate_random_fp(precision)
+            b = generate_random_fp(precision)
+            hex_a = float_to_llvm_hex(a, precision)
+            hex_b = float_to_llvm_hex(b, precision)
+            code += f"  %cmp{idx} = fcmp fast olt {llvm_type} {hex_a}, {hex_b}\n"
+            code += f"  %cmp_int{idx} = zext i1 %cmp{idx} to i32\n"
+
+        code += f"  %acc_val0 = load i32, i32* %acc\n"
+
+        for idx in range(unrolled):
+            code += f"  %acc_val{idx+1} = add i32 %acc_val{idx}, %cmp_int{idx}\n"
+
+        code += f"""
+  store i32 %acc_val{unrolled}, i32* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
 
 exit:
-  %final_cmp = load i1, i1* %acc
-  call void @use(i1 %final_cmp)
+  %final_acc = load i32, i32* %acc
+  call void @use_i32(i32 %final_acc)
   ret i32 0
 }}
 
-define void @use(i1 %val) {{
+define void @use_i32(i32 %val) {{
   ret void
 }}
 """
@@ -214,8 +238,8 @@ define void @use(i1 %val) {{
             return ""
         src_type = llvm_type
         dst_type = {"double": "float", "float": "half"}[precision]
-        a = generate_random_fp(precision)
-        hex_a = float_to_llvm_hex(a, precision)
+        random_fps = [generate_random_fp(precision) for _ in range(unrolled)]
+        hex_fps = [float_to_llvm_hex(f, precision) for f in random_fps]
         code = f"""
 define i32 @main() {{
 entry:
@@ -231,10 +255,15 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %result = fptrunc {src_type} {hex_a} to {dst_type}
-  %acc_val = load {dst_type}, {dst_type}* %acc
-  %new_acc = fadd fast {dst_type} %acc_val, %result
-  store {dst_type} %new_acc, {dst_type}* %acc
+  %acc_val0 = load {dst_type}, {dst_type}* %acc
+"""
+
+        for idx, hex_a in enumerate(hex_fps):
+            code += f"  %result{idx} = fptrunc {src_type} {hex_a} to {dst_type}\n"
+            code += f"  %acc_val{idx+1} = fadd fast {dst_type} %acc_val{idx}, %result{idx}\n"
+
+        code += f"""
+  store {dst_type} %acc_val{len(hex_fps)}, {dst_type}* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
@@ -254,8 +283,8 @@ define void @use({dst_type} %val) {{
             return ""
         src_type = llvm_type
         dst_type = {"float": "double", "half": "float"}[precision]
-        a = generate_random_fp(precision)
-        hex_a = float_to_llvm_hex(a, precision)
+        random_fps = [generate_random_fp(precision) for _ in range(unrolled)]
+        hex_fps = [float_to_llvm_hex(f, precision) for f in random_fps]
         code = f"""
 define i32 @main() {{
 entry:
@@ -271,10 +300,15 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %result = fpext {src_type} {hex_a} to {dst_type}
-  %acc_val = load {dst_type}, {dst_type}* %acc
-  %new_acc = fadd fast {dst_type} %acc_val, %result
-  store {dst_type} %new_acc, {dst_type}* %acc
+  %acc_val0 = load {dst_type}, {dst_type}* %acc
+"""
+
+        for idx, hex_a in enumerate(hex_fps):
+            code += f"  %result{idx} = fpext {src_type} {hex_a} to {dst_type}\n"
+            code += f"  %acc_val{idx+1} = fadd fast {dst_type} %acc_val{idx}, %result{idx}\n"
+
+        code += f"""
+  store {dst_type} %acc_val{len(hex_fps)}, {dst_type}* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
@@ -298,35 +332,23 @@ def generate_llvm_function_call(function_name, precision, iterations):
     llvm_type = precision_to_llvm_type[precision]
     intrinsic_suffix = precision_to_intrinsic_suffix[precision]
     code = ""
-    a = generate_random_fp(precision)
-    hex_a = float_to_llvm_hex(a, precision)
-    if function_name in {"pow", "hypot", "fma"}:
-        b = generate_random_fp(precision)
-        hex_b = float_to_llvm_hex(b, precision)
     if function_name == "pow":
         function_intrinsic = f"llvm.pow.{intrinsic_suffix}"
         code += f"declare {llvm_type} @{function_intrinsic}({llvm_type}, {llvm_type})\n"
-        function_call = f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {hex_a}, {llvm_type} {hex_b})"
-    elif function_name == "hypot":
-        code += f"declare {llvm_type} @{function_name}({llvm_type}, {llvm_type})\n"
-        function_call = f"call fast {llvm_type} @{function_name}({llvm_type} {hex_a}, {llvm_type} {hex_b})"
+        function_call_template = (
+            f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {{arg1}}, {llvm_type} {{arg2}})"
+        )
     elif function_name == "fma":
-        b = generate_random_fp(precision)
-        c = generate_random_fp(precision)
-        hex_b = float_to_llvm_hex(b, precision)
-        hex_c = float_to_llvm_hex(c, precision)
         function_intrinsic = f"llvm.fma.{intrinsic_suffix}"
         code += f"declare {llvm_type} @{function_intrinsic}({llvm_type}, {llvm_type}, {llvm_type})\n"
-        function_call = f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {hex_a}, {llvm_type} {hex_b}, {llvm_type} {hex_c})"
+        function_call_template = f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {{arg1}}, {llvm_type} {{arg2}}, {llvm_type} {{arg3}})"
+    elif function_name in functions_with_intrinsics:
+        function_intrinsic = f"llvm.{function_name}.{intrinsic_suffix}"
+        code += f"declare {llvm_type} @{function_intrinsic}({llvm_type})\n"
+        function_call_template = f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {{arg1}})"
     else:
-        if function_name in functions_with_intrinsics:
-            function_intrinsic = f"llvm.{function_name}.{intrinsic_suffix}"
-            code += f"declare {llvm_type} @{function_intrinsic}({llvm_type})\n"
-            function_call = f"call fast {llvm_type} @{function_intrinsic}({llvm_type} {hex_a})"
-        else:
-            code += f"declare {llvm_type} @{function_name}({llvm_type})\n"
-            function_call = f"call fast {llvm_type} @{function_name}({llvm_type} {hex_a})"
-
+        code += f"declare {llvm_type} @{function_name}({llvm_type})\n"
+        function_call_template = f"call fast {llvm_type} @{function_name}({llvm_type} {{arg1}})"
     code += f"""
 define i32 @main() {{
 entry:
@@ -342,10 +364,39 @@ loop:
   br i1 %cond, label %body, label %exit
 
 body:
-  %result = {function_call}
-  %acc_val = load {llvm_type}, {llvm_type}* %acc
-  %new_acc = fadd fast {llvm_type} %acc_val, %result
-  store {llvm_type} %new_acc, {llvm_type}* %acc
+  %acc_val0 = load {llvm_type}, {llvm_type}* %acc
+"""
+    for idx in range(unrolled):
+        if function_name == "pow":
+            a = generate_random_fp(precision)
+            b = generate_random_fp(precision)
+            hex_a = float_to_llvm_hex(a, precision)
+            hex_b = float_to_llvm_hex(b, precision)
+            function_call = function_call_template.format(arg1=hex_a, arg2=hex_b)
+        elif function_name == "hypot":
+            a = generate_random_fp(precision)
+            b = generate_random_fp(precision)
+            hex_a = float_to_llvm_hex(a, precision)
+            hex_b = float_to_llvm_hex(b, precision)
+            function_call = function_call_template.format(arg1=hex_a, arg2=hex_b)
+        elif function_name == "fma":
+            a = generate_random_fp(precision)
+            b = generate_random_fp(precision)
+            c = generate_random_fp(precision)
+            hex_a = float_to_llvm_hex(a, precision)
+            hex_b = float_to_llvm_hex(b, precision)
+            hex_c = float_to_llvm_hex(c, precision)
+            function_call = function_call_template.format(arg1=hex_a, arg2=hex_b, arg3=hex_c)
+        else:
+            a = generate_random_fp(precision)
+            hex_a = float_to_llvm_hex(a, precision)
+            function_call = function_call_template.format(arg1=hex_a)
+
+        code += f"  %result{idx} = {function_call}\n"
+        code += f"  %acc_val{idx+1} = fadd fast {llvm_type} %acc_val{idx}, %result{idx}\n"
+
+    code += f"""
+  store {llvm_type} %acc_val{unrolled}, {llvm_type}* %acc
   %i_next = add i32 %i_val, 1
   store i32 %i_next, i32* %i
   br label %loop
@@ -363,18 +414,39 @@ define void @use({llvm_type} %val) {{
     return code
 
 
-def compile_and_run(filename, executable):
-    compile_cmd = ["clang", "-O0", filename, "-lm", "-o", executable]
-    subprocess.run(compile_cmd, check=True)
-    start_time = time.perf_counter()
-    subprocess.run([f"./{executable}"], check=True)
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    return elapsed_time
+def compile_and_run(ll_filename, executable):
+    asm_filename = os.path.splitext(ll_filename)[0] + ".s"
+    try:
+        llc_cmd = ["llc", "-O0", ll_filename, "-o", asm_filename]
+        print(f"Compiling LLVM IR to assembly: {' '.join(llc_cmd)}")
+        subprocess.run(llc_cmd, check=True)
+        print(f"Assembly generated: {asm_filename}")
+
+        compile_cmd = ["clang", "-O0", asm_filename, "-lm", "-o", executable]
+        print(f"Compiling assembly to executable: {' '.join(compile_cmd)}")
+        subprocess.run(compile_cmd, check=True)
+        print(f"Executable generated: {executable}")
+
+        print(f"Running executable: ./{executable}")
+        start_time = time.perf_counter()
+        subprocess.run([f"./{executable}"], check=True)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"Execution time: {elapsed_time:.6f} seconds")
+
+        return elapsed_time
+
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred during compilation or execution: {e}")
+        raise
+
+    finally:
+        if os.path.exists(asm_filename):
+            print(f"Cleaned up assembly file: {asm_filename}")
 
 
 csv_file = "benchmark_results.csv"
-baseline_time = 0.0
+# baseline_time = 0.0
 
 with open(csv_file, "w", newline="") as csvfile:
     fieldnames = ["instruction", "precision", "cost"]
